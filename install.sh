24c729df89e86efe2b859fb6b6afd7e6ef43b46d
Thomson2412 <Thomasm.w.fink@gmail.com>
2022-04-19 12:22:31 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-conda_dir=$HOME/miniconda
+conda_dir=$HOME/miniconda3
 
 mkdir install_files
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O install_files/miniconda.sh
 chmod +x install_files/miniconda.sh
-rm -rf $HOME/miniconda
+rm -rf ${conda_dir}
 ./install_files/miniconda.sh -b -p ${conda_dir}
 
 conda_cmd="${conda_dir}/bin/conda"
 ${conda_cmd} init
 source "$HOME/.bashrc"
-source "$HOME/miniconda/bin/activate"
+source "${conda_dir}/bin/activate"
 conda config --set auto_activate_base false
 
 export CONDA_ALWAYS_YES="true"
@@ -27,10 +27,10 @@ if [[ $CONDA_DEFAULT_ENV == "keywordsearch" ]]; then
     wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm -O data/models/deepspeech-0.9.3-models.pbmm
     wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer -O data/models/deepspeech-0.9.3-models.scorer
 
-    echo "[Desktop Entry]
+    echo '[Desktop Entry]
 Name=Keywordsearch
-Exec=python3 $(pwd)/main.py
-Type=Application" >> $HOME/.local/share/applications/keywordsearch.desktop
+Exec=bash -c "cd $(pwd); source ${conda_dir}/bin/activate; cd /mnt/datadrive/work/keywordsearch; conda activate keywordsearch; python3 main.py" conda activate keywordsearch; python3 main.py"
+Type=Application' >> $HOME/.local/share/applications/keywordsearch.desktop
 fi
 unset CONDA_ALWAYS_YES
 
