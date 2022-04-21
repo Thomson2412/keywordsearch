19b7c55365ce7dcd3645fee29ad14faa1a5f1d7b
Thomson2412 <Thomson2412@users.noreply.github.com>
2022-04-21 17:50:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ sudo apt-get install python3-tk -y
 python3 -m venv venv
 source venv/bin/activate
 pip install .
+rm -rf data/models
+mkdir -p data/models
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm -O data/models/deepspeech-0.9.3-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer -O data/models/deepspeech-0.9.3-models.scorer
 echo "[Desktop Entry]
 Name=Keywordsearch
 Exec=bash -c 'cd $(pwd); source venv/bin/activate; python3 main.py'
