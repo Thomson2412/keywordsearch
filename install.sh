00964bd1c3e51d33bd45854c71158f93db9b79e2
Thomson2412 <Thomson2412@users.noreply.github.com>
2022-04-21 17:37:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo apt install python3-venv
+sudo apt install python3-pip
 python3 -m venv venv
-pip isntall .
+pip install .
 echo "[Desktop Entry]
 Name=Keywordsearch
 Exec=bash -c 'cd $(pwd); source venv/bin/activate; python3 main.py'
