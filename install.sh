cbf9a8421bd53b01c260668fd0b0e3e4aa3726be
Thomson2412 <Thomasm.w.fink@gmail.com>
2022-04-28 17:17:45 +0200

    Change desktop file location

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0
 echo "[Desktop Entry]
 Name=Keywordsearch
 Exec=bash -c 'cd $(pwd); source venv/bin/activate; python3 main.py'
-Type=Application" > $HOME/.local/share/applications/keywordsearch.desktop
+Type=Application" > $HOME/Desktop/keywordsearch.desktop
 
 
